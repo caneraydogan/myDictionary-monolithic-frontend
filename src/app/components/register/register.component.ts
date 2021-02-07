b02f8ce0f142fbade7e5a13cfc42513651006de5
caner <caneraydogan@gmail.com>
2021-02-07 18:04:33 +0100

    invitationCode

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -32,7 +32,8 @@ export class RegisterComponent implements OnInit {
             firstName: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
             lastName: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
             email: ['', Validators.compose([Validators.required, Validators.email])],
-            password: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
+            password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+            invitationCode: ['', Validators.required]
         });
     }
 
